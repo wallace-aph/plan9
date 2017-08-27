282ed346cf59bff1e939f3f1d1704dc4ad4bcc26
Wallace <ftubuntu@gmail.com>
2017-08-27 16:40:23 -0400

    clear program
    A better clear script than the one in the 9front's 9fs, as this one measures how many characters need to be cleared rather than clearing 100000 and hoping it's enough.

---
+++ clear.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a, i;
+	FILE * f = fopen("/dev/text", "r");
+	while(!feof(f)){
+		fgetc(f);
+		a++;
+	}
+	fclose(f);
+    for(i = 0; i <= a; i++){
+            printf("\x08");
+    }
+    return 0;
+}
