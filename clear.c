f7e6f50e53e0a808240d8a736a687369c96405a3
Wallace <ftubuntu@gmail.com>
2017-08-28 14:37:03 -0400

    Update clear.c

--- clear.c
+++ clear.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-	int a, i;
 	FILE * f = fopen("/dev/text", "r");
 	while(!feof(f)){
 		fgetc(f);
-		a++;
-	}
-	fclose(f);
-	for(i = 0; i <= a; i++){
 		printf("\x08");
 	}
+	fclose(f);
 	return 0;
 }
