b48c50910cddec6b3a9ada7e2dc28df325d1d405
Wallace <ftubuntu@gmail.com>
2017-08-28 15:00:18 -0400

    Update clear.c

--- clear.c
+++ clear.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
+	int a, i;
 	FILE * f = fopen("/dev/text", "r");
-	while(fgetc(f) != eof){
-		printf("\x08");
+	
+	// 'fgetc(f) != EOF' advances the stream, whereas feof does not
+	
+	while(fgetc(f) != EOF){
+		a++;
 	}
 	fclose(f);
+	
+	// originally I had the printf \b in the fgetc while loop, but that  
+	// caused some errors, as you're technically writing to /dev/text
+	
+	for(int i = 0; i <= a; i++){
+		printf("\x08");
+	}
 	return 0;
 }
